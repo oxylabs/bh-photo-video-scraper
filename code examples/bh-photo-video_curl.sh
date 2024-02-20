ac73f32187374afa050efac274cc0d0989456913
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2024-02-20 14:49:35 +0200

    Adding code examples/bh-photo-video_curl.sh

---
+++ code examples/bh-photo-video_curl.sh
@@ -0,0 +1,4 @@
+curl --user user:pass \
+'https://realtime.oxylabs.io/v1/queries' \
+-H "Content-Type: application/json" \
+-d '{"source": "universal_ecommerce", "url": "https://www.bhphotovideo.com/c/browse/photography/ci/989/n/4294538916?usource=lc&lc_aid=155352&gad_source=1&gclid=cjwkcaiaungubhakeiwagid4asfe6eo7ztftj_7yirnqc0yembejfvvhmjow87_7xwmaenz4vpjibboc_ysqavd_bwe"}'
